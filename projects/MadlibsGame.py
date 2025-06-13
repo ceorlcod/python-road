7f8180b3b69b2019203850903abc1d5011f8019a
Kenneth Mayo <2242754@slu.edu.ph>
2025-06-14 07:30:05 +0800

    feat: add MadlibsGame.py in project folder

---
+++ projects/MadlibsGame.py
@@ -0,0 +1,17 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+# This solely purpose was to know the user-input in the PYTHON
+
+adjective1 = input("Enter an adjective 1 (description): ")
+noun1 = input("Enter a noun 1 (person, place, or thing): ")
+verb1 = input("Enter a verb 1 (ends with \"ing\"): ")
+adjective2 = input("Enter an adjective 2 (description): ")
+adjective3 = input("Enter an adjective 3 (description): ")
+
+print("")
+print(f"Today I went to a {adjective1} school.")
+print(f"In a school, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
